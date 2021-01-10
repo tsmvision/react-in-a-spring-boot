30d654b5cabdb16c23d46e449eaa384c1b4f9327
lukelee <lukelee01@gmail.com>
2021-01-09 20:20:48 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## React with Spring Boot
+
+build react code in frontend folder
+
+deploy react build files at the spring boot compile time.
+
+make sure that target/class/public includes react build files.
+
---
+++ src/main/java/com/example/reactinaspringboot/ReactinaspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactinaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactinaspringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactinaspringbootApplication.class, args);
+    }
+
+}
